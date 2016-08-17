1893748de6ff3d9030590606d6e1266b171ce642
Luc Cote <luccote@lucs-macbook-pro.local>
2016-08-17 14:24:48 -0400

    Initial Commit

---
+++ SM Schedule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SM Schedule
+//
+//  Created by Luc Cote on 8/17/16.
+//  Copyright © 2016 EduApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
